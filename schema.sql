0280a80633e78f5996c2c9c8824fdefd6e788a4c
Sergey <serj_bibox@mail.ru>
2023-02-15 16:52:26 +0600

    schema

--- schema.sql
+++ schema.sql
@@ -2,20 +2,23 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     role VARCHAR(255) NOT NULL,   
-    phone VARCHAR(255), NOT NULL UNIQUE, 
+    phone VARCHAR(255) NOT NULL UNIQUE, 
     password VARCHAR(255) NOT NULL,    
     email VARCHAR(255) UNIQUE,
     name VARCHAR(255),
     age INTEGER,
     sex VARCHAR(255),    
-    icon VARCHAR(255),
+    icon VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS client_data;
-CREATE TABLE client_data (
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    problem TEXT NOT NULL,
+    client_id INTEGER NOT NULL REFERENCES users(id),
+    psy_id INTEGER NOT NULL REFERENCES users(id),
+    date TIMESTAMP NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    session_notes TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
@@ -30,17 +33,16 @@ CREATE TABLE psy_data (
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-DROP TABLE IF EXISTS sessions;
-CREATE TABLE sessions (
+DROP TABLE IF EXISTS client_data;
+CREATE TABLE client_data (
     id SERIAL PRIMARY KEY,
-    client_id INTEGER NOT NULL REFERENCES users(id),
-    psy_id INTEGER NOT NULL REFERENCES users(id),
-    date TIMESTAMP NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    session_notes TEXT,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    problem TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+
 
 
 INSERT INTO users(role, phone, password)
